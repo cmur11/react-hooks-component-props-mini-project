8369cb84fb72e51b131b0cdc7bacd8f14e14f20f
Conor Murnane <conormurnane11@gmail.com>
2021-02-01 23:03:08 -0500

    commit tree 332090681f6f25a1c2783990eaa8062000d36dbc

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "./Header.js"
+import About from "./About.js"
+import ArticleList from "./ArticleList.js"
+
+
+// const header = blogData.name
+// const image = blogData.image
+// const about = blogData.about
+//^^^ can do these in the function instead of duplicating work
+
+
 
 console.log(blogData);
+// console.log(image)
+// console.log(about)
+//  console.log(posts)
 
 function App() {
+
   return (
     <div className="App">
+      <Header  name = {blogData.name} />
+      <About image = {blogData.image} about = {blogData.about}/>
+      <ArticleList posts = {blogData.posts}/>
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
     </div>
